d0c5e6e8dce0b2399eb458f6e52340077ca89a68
ShevchukVA <60571733+ShevchukVA@users.noreply.github.com>
2020-12-05 17:25:22 +0200

    Updates

---
+++ precache-manifest.849032314d89c516ef0be8c77eac8856.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e853c04c4af78802412526663b388de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca35a83a62c112d6f7a9",
+    "url": "/static/css/7.77abdf47.chunk.css"
+  },
+  {
+    "revision": "9bdee101a13ce8200bb4",
+    "url": "/static/css/8.5bccf8bf.chunk.css"
+  },
+  {
+    "revision": "02d905b543fa44e722cc",
+    "url": "/static/css/contacts-view.c2240967.chunk.css"
+  },
+  {
+    "revision": "2f53044999cf940f56ea",
+    "url": "/static/css/login-view.8e5583e9.chunk.css"
+  },
+  {
+    "revision": "16eabbf705ab81460378",
+    "url": "/static/css/main.ed908955.chunk.css"
+  },
+  {
+    "revision": "905ef35bf914a3d87495",
+    "url": "/static/css/register-view.8e5583e9.chunk.css"
+  },
+  {
+    "revision": "ca35a83a62c112d6f7a9",
+    "url": "/static/js/7.1ab819b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/7.1ab819b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bdee101a13ce8200bb4",
+    "url": "/static/js/8.f2ec0477.chunk.js"
+  },
+  {
+    "revision": "02d905b543fa44e722cc",
+    "url": "/static/js/contacts-view.849a189f.chunk.js"
+  },
+  {
+    "revision": "7235b702cdcdbec9c4dc",
+    "url": "/static/js/home-view.cc7bb734.chunk.js"
+  },
+  {
+    "revision": "2f53044999cf940f56ea",
+    "url": "/static/js/login-view.3324180f.chunk.js"
+  },
+  {
+    "revision": "16eabbf705ab81460378",
+    "url": "/static/js/main.8d44d94a.chunk.js"
+  },
+  {
+    "revision": "5247053e633759d12e54",
+    "url": "/static/js/not-found.2ddbd242.chunk.js"
+  },
+  {
+    "revision": "905ef35bf914a3d87495",
+    "url": "/static/js/register-view.edb10396.chunk.js"
+  },
+  {
+    "revision": "829c9c55fdfab6a147be",
+    "url": "/static/js/runtime-main.022eb5ab.js"
+  },
+  {
+    "revision": "853ad099a00dda25958b041336befa6b",
+    "url": "/static/media/Phoonebook.853ad099.png"
+  }
+]);
